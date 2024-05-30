8b38502904385569c59226fcca0da36992b81574
Sutherland Nele <51810575+sutherlandnele@users.noreply.github.com>
2024-05-31 09:32:36 +1000

    Delete cloudcode-cmis-admin-webapp/prestart.sh

--- cloudcode-cmis-admin-webapp/prestart.sh
+++
@@ -1,6 +0,0 @@
-#echo "Running pre-start script"
-#echo "SiteURL: $SITE_URL"
-
-#sed -i '/const hostname/ c\const hostname = "'$SITE_URL'";' /app/src/config/endpoints.js
-
-#cat ./src/config/endpoints.js
